3c957e512f14af12ae5da2841e61bdf1390d9d3a
Daniel Alejo <yourmediasoft@gmail.com>
2018-02-26 12:05:46 -0500

    Updating App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
 
+componentWillMount() {
+  $('div').hover(()=>{
+    $('h1').addClass('textLight')
+    $('body').addClass('bg')
+  },()=>{
+    $('h1').removeClass('textLight')
+    $('body').removeClass('bg')
+  })
+}
+
   render() {
     return (
-      <div>
-        <h1>Works!!</h1>
+      <div className="container">
+        <h1 className="intro">React JS</h1>
       </div>
     );
   }
